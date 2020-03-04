97b08157f42726c9e954b40e62288b7ec94701d9
Michael Petö <michael@petoe.me>
2020-03-04 14:16:21 +0100

    Call script with vyattacfg

--- vyos/scripts/setup.sh
+++ vyos/scripts/setup.sh
@@ -4,4 +4,4 @@ cp /config/scripts/network/vyos/scripts/vyos-postconfig-bootup.script /config/sc
 /config/scripts/vyos-postconfig-bootup.script
 
 # Make sure the task-scheduler is set.
-/config/scripts/network/vyos/scripts/set-task.sh
+sg vyattacfg -c /config/scripts/network/vyos/scripts/set-task.sh
--- vyos/scripts/task.sh
+++ vyos/scripts/task.sh
@@ -2,4 +2,7 @@
 # Pull new config from remote git repo.
 git --git-dir=/config/scripts/network/.git --work-tree=/config/scripts/network pull origin master
 
+# Replace bootup config with current one.
+cp /config/scripts/network/vyos/scripts/vyos-postconfig-bootup.script /config/scripts/vyos-postconfig-bootup.script
+
 # TODO: GENERATE AND LOAD CONFIG HERE.
